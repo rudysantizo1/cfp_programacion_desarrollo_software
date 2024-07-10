f7b20112c6305bc2fb5c5c4baa5f060c06ec1446
Rudy Alejandro Santizo Arriaga <rudysantizo@CFPBARTOLOMEAMBROSIO.COM>
2024-07-10 15:56:17 -0600

    Se actualizo datos y se agrego la carpeta node nde con una sub carpeta ejemplos

---
+++ Modulo 2/Node/ejemplos/calculate.js
@@ -0,0 +1,6 @@
+module.exports = {suma,resta,multiplicacion,division};
+
+function suma (x,y) {return x+y;}
+function resta(x,y) {return x-y}
+function multiplicacion(x,y) {return x*y}
+function division(x,y) {return x/y}
---
+++ Modulo 2/Node/ejemplos/index.js
@@ -0,0 +1,11 @@
+let {suma:funcionsuma,  resta:funcioresta, multiplicacion:funcionmult, division:funciondiv} = require('./calculate.js');
+
+
+// console.log(funcionsuma(2,3));
+// console.log(funcioresta(2,3));
+// console.log(funcionmult(2,3));
+// console.log(funciondiv(2,3));
+
+
+console.log(globalThis);
+
